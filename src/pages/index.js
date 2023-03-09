6dff2c13ec760992a7b0cff83e7b846dd90f7234
finnius25 <finneykoshy@gmail.com>
2023-03-09 06:09:45 -0600

    add framer motion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const [promptInput, setPromptInput] = useState("");
@@ -36,26 +37,33 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <div className="flex flex-col items-center justify-center my-10 gap-8">
-        <div className="flex flex-col items-center sm:items-start">
-          <h1 className="text-6xl font-bold">Daddy Issues</h1>
-          <p className="font-light text-gray-400 flex flex-col text-center sm:text-start">
-            {`Have daddy issues? Talk about your feelings to your AI father
+      <motion.div
+        initial={{ opacity: 0, x: -10 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, delay: 1 }}
+      >
+        <div className="flex flex-col items-center justify-center my-10 gap-8">
+          <div className="flex flex-col items-center sm:items-start">
+            <h1 className="text-6xl font-bold">Daddy Issues</h1>
+            <p className="font-light text-gray-400 flex flex-col text-center sm:text-start">
+              {`Have daddy issues? Talk about your feelings to your AI father
             figure. Daddy's here to help.`}
-          </p>
-        </div>
+            </p>
+          </div>
 
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="promptInput"
-            placeholder="What's on your mind?"
-            value={promptInput}
-            onChange={(e) => setPromptInput(e.target.value)}
-            className="active:border-blue-500 px-4 py-2 rounded-xl border-blue-400 border-2"
-          />
-        </form>
-      </div>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              name="promptInput"
+              placeholder="What's on your mind?"
+              value={promptInput}
+              onChange={(e) => setPromptInput(e.target.value)}
+              className="active:border-blue-500 px-4 py-2 rounded-xl border-blue-400 border-2"
+            />
+          </form>
+        </div>
+      </motion.div>
+
       <div className="flex flex-col gap-8 w-10/12 sm:w-6/12">
         {youText && (
           <div className="flex gap-5">
